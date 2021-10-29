fd9a564b68a6b918e1996bfc1837ace30e628e5c
Ivan <ivan.lek123@gmail.com>
2021-10-29 19:24:05 +0300

    [AGH-B3-V1.3] work version app

--- src/app/service/response-git.service.ts
+++ src/app/service/response-git.service.ts
@@ -1,39 +1,67 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
+import {
+  catchError,
+  concatMap,
+  map,
+  mergeMap,
+  switchMap,
+} from 'rxjs/operators';
+
 import { GIT_API_URL, tokenGit } from '../const';
-import { Observable, of } from 'rxjs';
-import { catchError, concatMap, map, mergeMap } from 'rxjs/operators';
-import { CommitRepo, Repo } from '../interfaces';
+import { CommitRepo, DataRepo, Repo } from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ResponseGitService {
-  public commits: any = [];
-
   private headers = {
     headers: {
       Authorization: `token ${tokenGit}`,
     },
   };
+  public commits: CommitRepo[] = [];
+  public repositories = new BehaviorSubject<Repo[]>([]);
+  public userProfile = new Subject();
+  public arrayRepos: Repo[] = [];
+  
   constructor(private http: HttpClient) {}
 
-  getRepository(): Observable<any> {
-    return this.http
-      .get(
+  getObservableRepositories() {
+    return this.repositories;
+  }
+
+  getUserProfile() {
+    return this.userProfile;
+  }
+
+  getRepository(): void {
+    this.arrayRepos = [];
+    this.http
+      .get<DataRepo>(
         `${GIT_API_URL}/search/repositories?q=stars%3A%3E0&sort=stars&per_page=15`,
         this.headers
       )
       .pipe(
-        mergeMap((res: any) => {
+        mergeMap((res) => {
           return res.items?.map((item: Repo) => item.url);
         }),
-        concatMap((url: any) => this.http.get(url, this.headers)),
+        concatMap((url: string) => this.http.get(url, this.headers)),
+        map((res) => {
+          return { ...res, flag: false };
+        }),
         catchError((err) => of(err))
-      );
+      )
+      .subscribe((res) => {
+        if (!res.error) {
+          this.arrayRepos.push(res);
+          this.repositories.next(this.arrayRepos);
+        }
+      });
   }
 
-  getCommits(login: string, nameRepo: string): Observable<any> {
+  getCommits(login: string, nameRepo: string): Observable<CommitRepo[]> {
     return this.http
       .get(
         `${GIT_API_URL}/repos/${login}/${nameRepo}/commits?per_page=5`,
@@ -44,19 +72,52 @@ export class ResponseGitService {
           res.map((item: CommitRepo) => {
             return { ...item, userName: login };
           })
-        )
+        ),
+        catchError((err) => of(err))
       );
   }
 
   cacheCommits(commits: CommitRepo[]): void {
-    commits.forEach((item) => this.commits.push(item));
+    if (commits.length) {
+      commits.forEach((item) => this.commits.push(item));
+    }
   }
 
-  getCacheCommit(login: string): any {
+  getCacheCommit(login: string): CommitRepo[] {
     return this.commits.filter((item: CommitRepo) => item.userName === login);
   }
 
   deleteRepoCacheCommits(login: string): void {
     this.commits.filter((item: CommitRepo) => item.userName !== login);
+  }
+
+  searchRepositories(value: string): void {
+    if (!value.trim()) {
+      this.getRepository();
+    } else {
+      this.arrayRepos = [];
+      this.http
+        .get<DataRepo>(
+          `${GIT_API_URL}/search/repositories?q=${value}+in:name&per_page=7`,
+          this.headers
+        )
+        .pipe(
+          switchMap((res: DataRepo) => res.items),
+          map((res) => {
+            return { ...res, flag: false };
+          })
+        )
+        .subscribe((res) => {
+          this.arrayRepos.push(res);
+          this.repositories.next(this.arrayRepos);
+        });
+    }
+  }
+  
+  getUser(): void {
+    this.http.get(`${GIT_API_URL}/user`, this.headers).subscribe(
+      (res) => this.userProfile.next(res),
+      (error) => error
+    );
   }
 }
