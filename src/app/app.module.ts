3a09756f5eaa8767edb464739b5962415a5e9c9a
Ivan <ivan.lek123@gmail.com>
2021-10-29 14:34:55 +0300

    [AGH-B3-V1.2] display commit and fixed watchers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CommitComponent } from './components/commit/commit.component';
+
 import { RepositoryComponent } from './components/repository/repository.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CommitComponent,
-    RepositoryComponent,
-    HeaderComponent,
-  ],
+  declarations: [AppComponent, RepositoryComponent, HeaderComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/service/response-git.service.ts
+++ src/app/service/response-git.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { GIT_API_URL, tokenGit } from '../const';
-import { Observable, of, Subject } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
-import { Repo } from '../interfaces';
+import { Observable, of } from 'rxjs';
+import { catchError, concatMap, map, mergeMap } from 'rxjs/operators';
+import { CommitRepo, Repo } from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ResponseGitService {
-  public repositories: any;
+  public commits: any = [];
+
   private headers = {
     headers: {
       Authorization: `token ${tokenGit}`,
@@ -17,25 +18,45 @@ export class ResponseGitService {
   };
   constructor(private http: HttpClient) {}
 
-  getRepository(): Observable<Repo[]> {
-    const responseWrongData = this.http
+  getRepository(): Observable<any> {
+    return this.http
       .get(
         `${GIT_API_URL}/search/repositories?q=stars%3A%3E0&sort=stars&per_page=15`,
         this.headers
       )
-      .pipe(map((res: any) => res.items));
-
-    // responseWrongData.subscribe((repositories: Repo[]) =>
-    //   repositories.forEach((item) =>
-    //     this.repositories.push(this.repeatedReq(item.url))
-    //   )
-    // );
-    // console.log(this.repositories);
-
-    return responseWrongData;
+      .pipe(
+        mergeMap((res: any) => {
+          return res.items?.map((item: Repo) => item.url);
+        }),
+        concatMap((url: any) => this.http.get(url, this.headers)),
+        catchError((err) => of(err))
+      );
   }
 
-  repeatedReq(url: string) {
-    return this.http.get(url, this.headers);
+  getCommits(login: string, nameRepo: string): Observable<any> {
+    return this.http
+      .get(
+        `${GIT_API_URL}/repos/${login}/${nameRepo}/commits?per_page=5`,
+        this.headers
+      )
+      .pipe(
+        map((res: any) =>
+          res.map((item: CommitRepo) => {
+            return { ...item, userName: login };
+          })
+        )
+      );
+  }
+
+  cacheCommits(commits: CommitRepo[]): void {
+    commits.forEach((item) => this.commits.push(item));
+  }
+
+  getCacheCommit(login: string): any {
+    return this.commits.filter((item: CommitRepo) => item.userName === login);
+  }
+
+  deleteRepoCacheCommits(login: string): void {
+    this.commits.filter((item: CommitRepo) => item.userName !== login);
   }
 }
